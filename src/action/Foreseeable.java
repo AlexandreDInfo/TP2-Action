b2ac56bdc3da7596312e284aa95776abfa001cb3
Alexandre Delassus <alexandre.d.info@gmail.com>
2015-09-30 11:50:34 +0200

    30-09-2015 11:50

--- src/action/Foreseeable.java
+++ src/action/Foreseeable.java
@@ -3,24 +3,76 @@ package action;
 public class Foreseeable implements Action {
 
 	private int step;
+	private boolean finished;
+	private boolean ready;
 	
+// 	Constructor
+	
+	public Foreseeable(int numberOfStep){
+		this.step = numberOfStep;
+	}
+	
+// Getter / Setter
+	
+	/*
+	 *	Getter of step
+	 *	@return step number of step
+	 */
+	public int getStep() {
+		return step;
+	}
+	
+	/*
+	 *	Setter of step
+	 *	@param step number of step
+	 */
+	public void setStep(int step) {
+		this.step = step;
+	}
+	
+	/*
+	 *	Getter of ready
+	 *	@return ready
+	 */
 	public boolean isReady()
 	{
-		return false;
-		
+		return ready;
 	}
-
+	
+	/*
+	 *	Setter of ready
+	 *	@param ready
+	 */
+	public void setReady(boolean ready) {
+		this.ready = ready;
+	}
+	
+	/*
+	 *	Getter of finished
+	 *	@return finished
+	 */
 	public boolean isFinished()
 	{
-		return true;
+		return finished;
 	}
 	
-	@Override
+	/*
+	 *	Setter of finished
+	 *	@param finished
+	 */
+	public void setFinished(boolean finished) {
+		this.finished = finished;
+	}
+	
+//	Methods
+	
+	/*
+	 *	Do a step of the Action
+	 *	@throws ActionFinishedException when the Foreseeable is finished
+	 */
 	public void doStep() throws ActionFinishedException {
-		step--;
-		throw(new ActionFinishedException());
+		setStep(getStep() - 1);
+		if(this.getStep() <= 0)
+			throw(new ActionFinishedException());
 	}
-	
-	
-	
 }
