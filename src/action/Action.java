d4322de1d1a042a876f86cb75407b2be8a92e002
Alexandre Delassus <alexandre.d.info@gmail.com>
2015-10-07 10:41:21 +0200

    7/10 10:41

--- src/action/Action.java
+++ src/action/Action.java
@@ -1,6 +1,10 @@
 package action;
 
 public abstract class Action {
+	
+	protected int step;
+	protected boolean ready;
+	protected boolean finished;
 
 	public void doStep() throws ActionFinishedException
 	{
@@ -10,5 +14,55 @@ public abstract class Action {
 	}
 	
 	public abstract void reallyDoOneStep();
-	public abstract boolean isFinished();
+	// Getter / Setter
+	
+		/**
+		 *	Getter of step
+		 *	@return step number of step
+		 */
+		public int getStep() {
+			return step;
+		}
+		
+		/**
+		 *	Setter of step
+		 *	@param step number of step
+		 */
+		public void setStep(int step) {
+			this.step = step;
+		}
+		
+		/**
+		 *	Getter of ready
+		 *	@return ready
+		 */
+		public boolean isReady()
+		{
+			return ready;
+		}
+		
+		/**
+		 *	Setter of ready
+		 *	@param ready
+		 */
+		public void setReady(boolean ready) {
+			this.ready = ready;
+		}
+		
+		/**
+		 *	Getter of finished
+		 *	@return finished
+		 */
+		public boolean isFinished()
+		{
+			return finished;
+		}
+		
+		/**
+		 *	Setter of finished
+		 *	@param finished
+		 */
+		public void setFinished(boolean finished) {
+			this.finished = finished;
+		}
 }
--- src/action/Foreseeable.java
+++ src/action/Foreseeable.java
@@ -27,64 +27,13 @@ public class Foreseeable extends Action {
 		this.finished = false;
 	}
 	
-// Getter / Setter
-	
-	/**
-	 *	Getter of step
-	 *	@return step number of step
-	 */
-	public int getStep() {
-		return step;
-	}
-	
-	/**
-	 *	Setter of step
-	 *	@param step number of step
-	 */
-	public void setStep(int step) {
-		this.step = step;
-	}
-	
-	/**
-	 *	Getter of ready
-	 *	@return ready
-	 */
-	public boolean isReady()
-	{
-		return ready;
-	}
-	
-	/**
-	 *	Setter of ready
-	 *	@param ready
-	 */
-	public void setReady(boolean ready) {
-		this.ready = ready;
-	}
-	
-	/**
-	 *	Getter of finished
-	 *	@return finished
-	 */
-	public boolean isFinished()
-	{
-		return finished;
-	}
-	
-	/**
-	 *	Setter of finished
-	 *	@param finished
-	 */
-	public void setFinished(boolean finished) {
-		this.finished = finished;
-	}
-	
 //	Methods
 	
 	/**
 	 *	Do a step of the Action
 	 */
 	public void reallyDoOneStep() {
-		setStep(getStep() - 1);
+		this.setReady(false);
+		this.setStep(getStep() - 1);
 	}
 }
